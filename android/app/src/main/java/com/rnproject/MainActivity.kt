00d71b3fcdd41b5b6610be78ff914bfb8adf476f
binbinzhu <binbin.zhu@opay-inc.com>
2019-10-12 19:44:17 +0800

    增加kotlin支持，增加测试类RnListActivity

---
+++ android/app/src/main/java/com/rnproject/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.rnproject
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+import com.facebook.react.ReactActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        develop.setOnClickListener {
+            startActivity(Intent(this@MainActivity,RnListActivity::class.java))
+        }
+        list.setOnClickListener {
+            startActivity(Intent(this@MainActivity,RnDevActivity::class.java))
+        }
+    }
+}
